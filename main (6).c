54fd906895b353ead67ffd211d468dc7c09b1ef4
RajeshGanes <166837090+RajeshGanes@users.noreply.github.com>
2024-04-12 20:15:28 +0530

    Add files via upload

---
+++ main (6).c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+ const int variable_number = 5000;
+ const float variable_pi = 3.14;
+ const char letter = 'A';
+ printf("Integer constant variable number with value %d\n",variable_number);
+ printf("Float constant variable pi with value %f\n,",variable_pi);
+ printf("char constant variable with value %c\n",letter);
+ getch();
+}
